24fcbc4677841ba166029265a2c335c59aa077a7
Kunal Chilka <kunaldchilka+github@gmail.com>
2019-07-06 16:13:57 -0700

    First commit

---
+++ LinkedList/LinkedListClass.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    public class LinkedListClass
+    {
+
+        public class Node
+        {
+            public int value;
+            public Node next;
+            public Node(int val)
+            {
+                this.value = val;
+                this.next = null;
+            }
+        }
+
+        public int count = 0;
+        private Node Head { get; set; }
+        private Node CurrNode { get; set; }
+
+        public void AddNode(Node newNode)
+        {
+            //if this is the first node
+            if (Head == null)
+            {
+                Head = newNode;
+                CurrNode = Head;
+
+            }
+            else
+            {
+                CurrNode.next = newNode;
+                CurrNode = newNode;
+            }
+
+            count++;
+        }
+
+
+        public void DeleteLastNode()
+        {
+            Node tempNode = Head;
+
+            while (tempNode.next != CurrNode)
+            {
+                tempNode = tempNode.next;
+            }
+
+            //now tempNode.Next = CurrentNode
+            CurrNode = tempNode;
+            CurrNode.next = null;
+            count--;
+
+            Console.WriteLine("item Deleted");
+            PrintList();
+        }
+
+
+        public void AddAfterValue(int afterVal, Node nodeToAdd)
+        {
+            if (Head == null)
+            {
+                Console.WriteLine("Invalid operation!");
+            }
+            else
+            {
+                Node tempNode = Head;
+                while (tempNode.value != afterVal)
+                {
+                    tempNode = tempNode.next;
+                }
+
+                Node nextNode = tempNode.next;
+                tempNode.next = nodeToAdd;
+                nodeToAdd.next = nextNode;
+            }
+
+            PrintList();
+        }
+
+
+        public void DeleteNode(int val)
+        {
+            Node tempNode = Head;
+            Node prevNode = Head;
+
+            while (tempNode.value != val)
+            {
+                prevNode = tempNode;
+                tempNode = tempNode.next;
+            }
+
+            Node nextNode = tempNode.next;
+            prevNode.next = nextNode;
+
+            Console.WriteLine("After deleting value: " + val);
+            PrintList();
+        }
+        
+        public void PrintList()
+        {
+            Node tempNode = Head;
+
+            while (tempNode != null)
+            {
+                Console.WriteLine(tempNode.value);
+                tempNode = tempNode.next;
+            }
+        }
+
+        public void DeleteWhoseSumIsZero()
+        {
+
+
+        }
+
+
+
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating LinkedList of 5");
+
+
+            LinkedListClass linkedList = new LinkedListClass();
+
+            LinkedListClass.Node newNodeToadd = new LinkedListClass.Node(10);
+            linkedList.AddNode(newNodeToadd);
+
+            LinkedListClass.Node newNodeToadd2 = new LinkedListClass.Node(20);
+            linkedList.AddNode(newNodeToadd2);
+
+            LinkedListClass.Node newNodeToadd3 = new LinkedListClass.Node(-30);
+            linkedList.AddNode(newNodeToadd3);
+
+            LinkedListClass.Node newNodeToadd4 = new LinkedListClass.Node(40);
+            linkedList.AddNode(newNodeToadd4);
+
+            LinkedListClass.Node newNodeToadd5 = new LinkedListClass.Node(50);
+            linkedList.AddNode(newNodeToadd5);
+
+            Console.WriteLine("Total number of items in the list: "+ linkedList.count);
+
+            linkedList.PrintList();
+
+            Console.WriteLine("deleting last item");
+
+            linkedList.DeleteLastNode();
+            
+            Console.WriteLine("Total number of items in the list after deleting: " + linkedList.count);
+            
+            linkedList.AddAfterValue(30, new LinkedListClass.Node(35));
+
+            linkedList.DeleteNode(35);
+
+
+
+
+        }
+    }
+}
